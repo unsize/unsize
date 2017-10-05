14d46fc0250ba8a4fd5f17ae6823ee0aab3ae237
Jon Corbett <corbett.jo@husky.neu.edu>
2017-10-04 21:09:04 -0400

    Update index.js

--- extension/index.js
+++ extension/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'route-lite';
-import Home from './components/Home';
 import Window from '../style/Window';
 import OnboardingWelcome from './screens/OnboardingWelcome';
 
