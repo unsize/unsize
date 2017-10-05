d8cba7317f7542372a7a41481b7f7895fb9c71ab
Jon Corbett <corbett.jo@husky.neu.edu>
2017-10-04 21:09:58 -0400

    Update Button.js

--- style/Button.js
+++ style/Button.js
@@ -2,17 +2,17 @@ import styled from 'styled-components';
 import { halisFont } from './constants';
 
 const Button = styled.button`
-font-family: ${halisFont};
-width: 100px;
-height: 30px;
-border-radius: 10px;
-background: none;
-border-style: solid;
-border-width: 1px;
-border-color: #000;
-margin: 20px auto;
-font-size: 16px;
-display: block;
+  font-family: ${halisFont};
+  width: 100px;
+  height: 30px;
+  border-radius: 10px;
+  background: none;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #000;
+  margin: 20px auto;
+  font-size: 16px;
+  display: block;
 `;
 
 export default Button;
