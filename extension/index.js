fa9a848c2cc315db05fec6ed820d5537d6b32216
Jon Corbett <corbett.jo@husky.neu.edu>
2017-10-05 15:37:08 -0400

    Update index.js

--- extension/index.js
+++ extension/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Router from 'route-lite';
 import Window from '../style/Window';
 import OnboardingWelcome from './screens/OnboardingWelcome';
+import 'style/fontFaces';
 
 ReactDOM.render(
   <Wrapper>
