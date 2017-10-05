1711b607543b14b9f033c9e6e32d45c44f95b832
Jon Corbett <corbett.jo@husky.neu.edu>
2017-10-04 21:56:12 -0400

    Update OnboardingWelcome.js

--- extension/screens/OnboardingWelcome.js
+++ extension/screens/OnboardingWelcome.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../../style/Logo';
 import H1 from '../../style/H1';
 import P from '../../style/P';
-import PrimaryButton from '../../style/PrimaryButton';
+import Button from '../../style/Button';
 
 export default class OnboardingWelcome extends React.Component {
 
@@ -12,7 +12,7 @@ export default class OnboardingWelcome extends React.Component {
         <Logo src="icon.png"/>
         <H1>Hi, welcome to unsize!</H1>
         <P>Ready to unbox yourself and find your perfect fit?</P>
-        <PrimaryButton>unsize me</PrimaryButton>
+        <Button primary>unsize me</Button>
         <P>Join the revolution and start shopping personally!</P>
       </div>
       );
