6cd5bcab01f0cab18f14db9fce5ea35b1838224f
Jon Corbett <corbett.jo@husky.neu.edu>
2017-10-05 15:52:26 -0400

    Update index.js

--- style/index.js
+++ style/index.js
@@ -1,4 +1,5 @@
 import Button from './Button';
+import Input from './Input';
 import Div from './Div';
 import Footer from './Footer';
 import H1 from './H1';
@@ -7,7 +8,9 @@ import H3 from './H3';
 import H4 from './H4';
 import H5 from './H5';
 import H6 from './H6';
+import P from './P';
+import Logo from './Logo';
 import Header from './Header';
 import Strong from './Strong';
 
-export { Button, Div, Footer, H1, H2, H3, H4, H5, H6, Header, Strong };
+export { Button, Input Div, Footer, H1, H2, H3, H4, H5, H6, P, Logo, Header, Strong };
