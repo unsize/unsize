0580348e8d2c84e99893ffd514f8d8e9f8bae7aa
aem <amarkon895@gmail.com>
2017-10-14 19:27:19 -0400

    combine input components

--- extension/screens/ManualEntry.js
+++ extension/screens/ManualEntry.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Button,
-  H1,
-  Logo,
-  NumberInput,
-  ProfileArea,
-  TwoColumnForm
-} from 'style';
+import { Button, H1, Logo, ProfileArea, TwoColumnForm } from 'style';
 
 export default class ManualEntry extends React.Component {
   render() {
@@ -18,7 +11,7 @@ export default class ManualEntry extends React.Component {
         <H1>{this.props.name}'s Measurements</H1>
         <TwoColumnForm>
           {measurements.map(function(placeholder, i) {
-            return <NumberInput placeholder={placeholder} key={i} />;
+            return <Input type="number" placeholder={placeholder} key={i} />;
           })}
           <Button>Unsize Me!</Button>
         </TwoColumnForm>
--- style/index.js
+++ style/index.js
@@ -13,11 +13,30 @@ import Header from './Header';
 import Image from './Image';
 import Input from './Input';
 import Logo from './Logo';
-import NumberInput from './NumberInput';
 import P from './P';
 import ProfileArea from './ProfileArea';
 import Strong from './Strong';
 import TwoColumnForm from './TwoColumnForm';
 import Window from './Window';
 
-export { Button, Div, Footer, Form, H1, H2, H3, H4, H5, H6, Header, Image, Input, Logo, NumberInput, ProfileArea, P, Strong, TwoColumnForm, Window };
+export {
+  Button,
+  Div,
+  Footer,
+  Form,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Header,
+  Image,
+  Input,
+  Logo,
+  ProfileArea,
+  P,
+  Strong,
+  TwoColumnForm,
+  Window
+};
