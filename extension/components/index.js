2257ac367de860ef44c8ad1d99e786e99f4a5be9
Josh Pensky <joshuapensky@gmail.com>
2017-11-08 10:33:12 -0500

    Removed WindowCorners reference in index file

--- extension/components/index.js
+++ extension/components/index.js
@@ -2,12 +2,10 @@ import BackProfileHeader from './BackProfileHeader';
 import Retailer from './Retailer';
 import ScreenContainer from './ScreenContainer';
 import Window from './Window';
-import WindowCorners from './WindowCorners';
 
 export {
   BackProfileHeader,
   Retailer,
   ScreenContainer,
-  Window,
-  WindowCorners
+  Window
 };
